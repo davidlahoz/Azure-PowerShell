03360f86252d83ba1e710a80a7491ccca7b51a08
David L <90243715+davidlahoz@users.noreply.github.com>
2024-03-14 14:45:28 +0100

    Update Set-Mailbox_TransformToSharedMailbox.ps1
    Needed modules not mentioned in comments

--- eMail accounts tasks/Set-Mailbox_TransformToSharedMailbox.ps1
+++ eMail accounts tasks/Set-Mailbox_TransformToSharedMailbox.ps1
@@ -1,3 +1,6 @@
+# Needed Modules:
+# ExchangeOnlineManagement - https://www.powershellgallery.com/packages/ExchangeOnlineManagement/
+
 # Connect to Exchange Online - prompts user-interactive login
 Connect-ExchangeOnline
 
